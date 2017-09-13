d293de97752265ce3f7e971ff5aab72f83c195c5
Brad Aylsworth <Brad.Aylsworth@gmail.com>
2017-09-13 01:03:20 -0500

    barebones plan

--- src/main.py
+++ src/main.py
@@ -6,45 +6,54 @@ class Character:
         self.name = name
         self.actor = actor
         self.charpic = Image.open(charpic)
-        self.actorpic = Image.open(actorpic)
+        self.actorpic = Image.open(actorpic).resize(
+                            (315,600), Image.ANTIALIAS)
 
 keyleth = Character('Keyleth', 'Marisha Ray',
                         'characters/pc/Keyleth/Keyleth2.png',
                         'characters/pc/Keyleth/Ray.jpg')
 
-master = tk.Tk()
-
-tkimage = ImageTk.PhotoImage(keyleth.charpic)
-char = tk.Label(master, image=tkimage)
-char.pack()
-
-master.mainloop()
-exit()
-
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+        self.playing = False
+
         self.initUI()
+
+    def toggle(self):
+        if self.playing:
+            self.subs['text'] = "paused"
+            self.playing = False
+        else:
+            self.subs['text'] = "playing"
+            self.playing = True
 
     def initUI(self):
         self.master.title("Critrole-Faces - ")
 
-        char = tk.Label(self, image=ImageTk.PhotoImage(keyleth.charpic))
-        char.image = keyleth.charpic #necessary?
+        tkchar = ImageTk.PhotoImage(keyleth.actorpic)
+        self.actor = tk.Label(self, image=tkchar)
+        self.actor.image = tkchar
+        self.actor.grid(column=0, row=0)
 
-        char.pack()
-        self.pack()
+        tkchar = ImageTk.PhotoImage(keyleth.charpic)
+        self.char = tk.Label(self, image=tkchar)
+        self.char.image = tkchar
+        self.char.grid(column=1, row=0)
 
-    def setgeometry(self):
-        w, h = keyleth.charpic.size
-        self.master.geometry("{}x{}+300+300".format(w, h))
+        self.subs = tk.Label(self, text="paused")
+        self.subs.grid(column=0, columnspan=2, row=1)
+
+        self.playpause = tk.Button(self, text="play/pause", command=self.toggle)
+        self.playpause.grid(column=0, columnspan=2, row=2)
+
+        self.grid()
         
 
 def main():
     root = tk.Tk()
     app = Application()
-    app.setgeometry()
     root.mainloop()
 
 if __name__ == '__main__':
---
+++ src/plan.md
@@ -0,0 +1,25 @@
+# The plan
+
+Assuming Nick puts his bars on the bottom, it could be (915 x 980 +0 +0)
+
+### picker
+
+This'll might be cli, at least at first.
+
+### Faces frame
+
+915 x 600 + ~80
+
+Put actor and character name across the top, then have a thin actor photo left of the square character art.
+
+### Transcript frame
+
+915 x 200
+
+The subtitles. will either be complicated or there'll be a python library for it.
+
+### Controller frame
+
+915 x 100
+
+progress bar, play/pause, etc.
