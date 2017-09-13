5725fd163efd739c5d83533a8e4f32e2c1d9a2a3
Brad Aylsworth <Brad.Aylsworth@gmail.com>
2017-09-12 22:44:10 -0500

    check on linux

--- src/main.py
+++ src/main.py
@@ -1,20 +1,52 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
-class CritroleFaces(tk.Frame):
+class Character:
+    def __init__(self, name, actor, charpic, actorpic):
+        self.name = name
+        self.actor = actor
+        self.charpic = Image.open(charpic)
+        self.actorpic = Image.open(actorpic)
+
+keyleth = Character('Keyleth', 'Marisha Ray',
+                        'characters/pc/Keyleth/Keyleth2.png',
+                        'characters/pc/Keyleth/Ray.jpg')
+
+print(keyleth.actorpic.size)
+print(keyleth.charpic.size)
+
+master = tk.Tk()
+
+char = tk.Label(master, image=ImageTk.PhotoImage(keyleth.charpic))
+char.pack()
+
+master.mainloop()
+exit()
+
+
+class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        self.init()
+        self.initUI()
 
-    def init(self):
+    def initUI(self):
         self.master.title("Critrole-Faces - ")
-        self.pack(fill=tk.BOTH, expand=1)
+
+        char = tk.Label(self, image=ImageTk.PhotoImage(keyleth.charpic))
+        char.image = keyleth.charpic #necessary?
+
+        char.pack()
+        self.pack()
+
+    def setgeometry(self):
+        w, h = keyleth.charpic.size
+        self.master.geometry("{}x{}+300+300".format(w, h))
         
 
 def main():
     root = tk.Tk()
-    root.geometry("250x150+300+300")
-    app = CritroleFaces()
+    app = Application()
+    app.setgeometry()
     root.mainloop()
 
 if __name__ == '__main__':
